49122cabdf996c52f060f2c0a35988fdbc202221
YasmineBaati <yasminebaati321@gmail.com>
2023-05-16 13:22:55 +0100

    Hello_World100%

---
+++ Python/Week 1/Day 1/Practice/Hello_World.py
@@ -0,0 +1,11 @@
+print("Hello World")
+name="Yasmine"
+print("Hello Yasmine!")
+print("Hello " + name + "!")
+name=3
+print("Hello 3!")
+print("Hello " + str(name)+"!")
+fav_food1="pasta"
+fav_food2="pizza"
+print("i love to eat " + fav_food1 + " and " + fav_food2)
+print("i love to eat {} and {}".format(fav_food1,fav_food2))
